62014a91faff257a5d3d31957df0bfe1d78cf039
minsub <chrkb1569@naver.com>
2024-02-18 17:06:22 +0900

    feat : Init Project

---
+++ src/test/java/com/chrkb1569/CharacterInfo/CharacterInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrkb1569.CharacterInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
